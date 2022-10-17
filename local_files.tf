3a75951b74b8438cd1d04d07869635d2187e015f
Palak Bhawsar <69889600+palakbhawsar98@users.noreply.github.com>
2022-10-17 17:47:56 +0530

    Create local_files.tf

---
+++ local_files.tf
@@ -0,0 +1,13 @@
+resource "local_file" "users"
+{
+  filename = "/root/config_files/user.txt"
+  content = "User details - Anna, Sally, John"
+}
+
+resource "aws_instance" "jenkins server"
+{
+  ami = "amibvgg545465"
+  instance_type = "t3.micro"
+  tags = {
+    Name = "JenkinsServer"
+ }
