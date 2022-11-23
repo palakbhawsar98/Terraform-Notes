a57e007a59731a8ab4df9593bf3a94bb89fa9a29
Palak Bhawsar <69889600+palakbhawsar98@users.noreply.github.com>
2022-11-23 21:42:59 +0530

    Update variables.tf

--- terraform-remote-state-hands-on/variables.tf
+++ terraform-remote-state-hands-on/variables.tf
@@ -1,10 +1,10 @@
-variables "ami-name" {
+variable "ami-name" {
    description = "Name of an AMI"
    type = string
    default = "ami-08c40ec9ead489470"
 }
 
-variables "instance-size" {
+variable "instance-size" {
    description = "Size of an Instance"
    type = string
    default = "t2.micro"
